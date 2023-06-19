66d624f94cf0ec0e3caf45a6b19d890d43d65484
Temirzhan Yussupov <timka2609@gmail.com>
2023-06-19 14:51:35 +0600

    feat: implement SgBridge

--- contracts/SgBridge.sol
+++ contracts/SgBridge.sol
@@ -2,55 +2,161 @@
 
 pragma solidity ^0.8.18;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-import {IStargateRouter} from "./interfaces/IStargate.sol";
+import {IStargateRouter, IStargateReceiver} from "./interfaces/IStargate.sol";
 import {ISgBridge} from "./interfaces/ISgBridge.sol";
 
-contract SgBridge is Ownable, ISgBridge {
+contract SgBridge is OwnableUpgradeable, ISgBridge, IStargateReceiver {
+    using SafeERC20 for IERC20;
+
     IStargateRouter public router;
 
-    uint256 public slippage = 9_900;
-    uint256 public dstGasForCall = 500_000;
+    uint256 public slippage;
+    uint256 public dstGasForCall;
+    uint16 public currentChainId;
 
-    constructor(IStargateRouter _router) {
-        router = _router;
+    mapping(address => mapping(uint16 => uint256)) public poolIds;
+    mapping(uint16 => address) public supportedDestinations;
+
+    function initialize(address _router) public override initializer {
+        __Ownable_init();
+
+        router = IStargateRouter(_router);
+        slippage = 9_900;
+        dstGasForCall = 500_000;
     }
 
     function setSlippage(uint256 _slippage) external override onlyOwner {
         slippage = _slippage;
     }
 
+    function setDstGasForCall(
+        uint256 _dstGasForCall
+    ) external override onlyOwner {
+        dstGasForCall = _dstGasForCall;
+    }
+
+    function setCurrentChainId(
+        uint16 _currentChainId
+    ) external override onlyOwner {
+        currentChainId = _currentChainId;
+    }
+
+    function setStargatePoolId(
+        address token,
+        uint16 chainId,
+        uint256 poolId
+    ) external override onlyOwner {
+        IERC20(token).safeApprove(address(router), type(uint256).max);
+        poolIds[token][chainId] = poolId;
+    }
+
+    function setSupportedDestination(
+        uint16 chainId,
+        address receiveContract
+    ) external override onlyOwner {
+        supportedDestinations[chainId] = receiveContract;
+    }
+
+    function bridge(
+        address token,
+        uint256 amount,
+        uint16 destChainId,
+        address destinationAddress,
+        bytes memory message
+    ) external payable override {
+        uint256 destinationPool = poolIds[token][destChainId];
+        if (destinationPool == 0) {
+            revert TokenNotSupported(token, destChainId);
+        }
+
+        uint256 sourcePool = poolIds[token][currentChainId];
+        if (sourcePool == 0) {
+            revert TokenNotSupported(token, currentChainId);
+        }
+
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        if (currentChainId == destChainId) {
+            IERC20(token).safeTransfer(destinationAddress, amount);
+            return;
+        }
+
+        address receiveContract = supportedDestinations[destChainId];
+        if (receiveContract == address(0)) {
+            revert DestinationNotSupported(destChainId);
+        }
+
+        _bridgeInternal(
+            amount,
+            sourcePool,
+            destChainId,
+            destinationPool,
+            destinationAddress,
+            receiveContract,
+            message
+        );
+    }
+
+    function sgReceive(
+        uint16,
+        bytes memory,
+        uint,
+        address token,
+        uint256 amountLD,
+        bytes memory payload
+    ) external override {
+        require(msg.sender == address(router), "SgBridge::Forbidden");
+
+        (address toAddr, bytes memory message) = abi.decode(
+            payload,
+            (address, bytes)
+        );
+
+        IERC20(token).safeTransfer(toAddr, amountLD);
+        (bool success, ) = toAddr.call(message);
+
+        emit SgReceived(token, amountLD, success);
+    }
+
     function _bridgeInternal(
-        uint256 fee,
         uint256 amount,
         uint256 srcPoolId,
         uint16 destChainId,
         uint256 destinationPoolId,
         address destinationAddress,
-        address destinationToken,
-        address destinationContract
+        address destinationContract,
+        bytes memory message
     ) internal {
-        IStargateRouter.LzTxObj memory lzParams = IStargateRouter.LzTxObj({
-            dstGasForCall: dstGasForCall,
-            dstNativeAmount: 0,
-            dstNativeAddr: abi.encode(address(this))
-        });
+        bytes memory payload = abi.encode(destinationAddress, message);
+        uint256 withSlippage = (amount * slippage) / 10_000;
 
-        bytes memory payload = abi.encode(destinationAddress, destinationToken);
-
-        router.swap{value: fee}(
+        router.swap{value: msg.value}(
             destChainId,
             srcPoolId,
             destinationPoolId,
             payable(msg.sender),
             amount,
-            0,
-            lzParams,
+            withSlippage,
+            _getLzParams(),
             abi.encodePacked(destinationContract),
             payload
         );
 
         emit Bridge(destChainId, amount);
+    }
+
+    function _getLzParams()
+        internal
+        view
+        returns (IStargateRouter.LzTxObj memory)
+    {
+        return
+            IStargateRouter.LzTxObj({
+                dstGasForCall: dstGasForCall,
+                dstNativeAmount: 0,
+                dstNativeAddr: abi.encode(address(this))
+            });
     }
 }
--- contracts/interfaces/ISgBridge.sol
+++ contracts/interfaces/ISgBridge.sol
@@ -5,7 +5,36 @@ pragma solidity ^0.8.18;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ISgBridge {
+    error TokenNotSupported(address token, uint16 destChainId);
+    error DestinationNotSupported(uint16 destChainId);
+
     event Bridge(uint16 indexed chainId, uint256 amount);
+    event SgReceived(address indexed token, uint256 amount, bool success);
+
+    function initialize(address _router) external;
 
     function setSlippage(uint256 _slippage) external;
+
+    function setDstGasForCall(uint256 _dstGasForCall) external;
+
+    function setCurrentChainId(uint16 _currentChainId) external;
+
+    function setStargatePoolId(
+        address token,
+        uint16 chainId,
+        uint256 poolId
+    ) external;
+
+    function setSupportedDestination(
+        uint16 chainId,
+        address receiveContract
+    ) external;
+
+    function bridge(
+        address token,
+        uint256 amount,
+        uint16 destChainId,
+        address destinationAddress,
+        bytes memory message
+    ) external payable;
 }
