558974ed1709276d0534f578d184a80394637fbc
GoldenSylph <simplavero@gmail.com>
2023-09-14 16:03:24 +0400

    remove ref

--- contracts/strategies/ref/discountHub/interfaces/IRewardDistributor.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.15;
-
-interface IRewardDistributor {
-    function setDistributionPartitionBasePoints(uint256 newBasePoints) external;
-    function getReward() external;
-    function getRewardForCustodial(address receiver) external; // back
-}
