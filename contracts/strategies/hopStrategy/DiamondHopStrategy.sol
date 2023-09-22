11455848c9b7996055e8e3108662955db4699277
GoldenSylph <simplavero@gmail.com>
2023-09-22 15:33:48 +0400

    fix typo

--- contracts/strategies/hopStrategy/DiamondHopStrategy.sol
+++ contracts/strategies/hopStrategy/DiamondHopStrategy.sol
@@ -8,7 +8,7 @@ import "./v1/interfaces/IHSInitializerFacet.sol";
 
 // IMPORTANT: all of the collective diamond interfaces MUST be prefixed with Diamond word.
 /// @dev This MUST aggregate all of the faucets interfaces, to be able to grasp a full view of ABI in one place.
-interface DiamondHS is
+interface DiamondHopStrategy is
     IHSHarvestFacet,
     IHSStatsFacet,
     IHSInitializerFacet
