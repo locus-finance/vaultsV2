7571f02795659b29f769802971405de483f0e661
GoldenSylph <simplavero@gmail.com>
2023-09-27 13:20:36 +0400

    remove duplicate event

--- contracts/strategies/baseStrategy/v1/swaps/BSQuoteNotifiableFacet.sol
+++ contracts/strategies/baseStrategy/v1/swaps/BSQuoteNotifiableFacet.sol
@@ -4,22 +4,16 @@ pragma solidity ^0.8.18;
 
 import "../../../baseStrategy/v1/interfaces/IBSQuoteNotifiableFacet.sol";
 import "../../../diamondBase/facets/BaseFacet.sol";
+import "./libraries/BSOneInchLib.sol";
 import "../../BSLib.sol";
 
 contract BSQuoteNotifiableFacet is BaseFacet, IBSQuoteNotifiableFacet {
-    event QuoteNotified(
-        address indexed src,
-        address indexed dst,
-        uint256 indexed amountOut,
-        uint256 amountIn
-    );
-
     function notifyCallback(
         address src,
         address dst,
         uint256 amountOut,
         uint256 amountIn
     ) external virtual override internalOnly {
-        emit QuoteNotified(src, dst, amountOut, amountIn);
+        emit BSOneInchLib.Notified(src, dst, amountOut, amountIn);
     }
 }
