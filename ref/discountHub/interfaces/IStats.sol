fff105bdadaa464c509a71c123837dc58d64c8bb
GoldenSylph <simplavero@gmail.com>
2023-09-27 12:37:55 +0400

    remove ref

--- ref/discountHub/interfaces/IStats.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.15;
-
-interface IStats {
-    function totalDeposit(address outputToken) external view returns (uint256);
-
-    function totalPoolReward(address rewardToken) external view returns (uint256);
-}
