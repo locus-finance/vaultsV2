2494eb391a508f13ccc9a8353491ad6f920e3adf
GoldenSylph <simplavero@gmail.com>
2023-09-28 16:55:50 +0400

    added final script for hop diamond deploy

--- deploy/stages/substages/hopStrategyDiamondDeploy.js
+++ deploy/stages/substages/hopStrategyDiamondDeploy.js
@@ -6,6 +6,14 @@ module.exports = async ({
 }) => {
   const { diamond } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  const bridgeConfig = require("../../constants/bridgeConfig.json");
+  const { vaultChain } = require("../../utils");
+  const config = bridgeConfig["arbitrumOne"];
+  const vaultConfig = bridgeConfig[vaultChain("arbitrumOne")];
+
+  const TOKEN = "USDC";
+
   await diamond.deploy('HopStrategy', {
     from: deployer,
     owner: deployer,
@@ -36,7 +44,28 @@ module.exports = async ({
     execute: {
       methodName: 'initialize',
       args: [
-
+        [config.lzEndpoint],
+        [
+          deployer,
+          config[TOKEN].address,
+          vaultConfig.vault,
+          vaultConfig.chainId,
+          vaultConfig.chainId,
+          config.slippage
+        ],
+        [
+          config.sgBridge,
+          config.sgRouter
+        ],
+        [
+          140,
+          1100,
+          "0x1111111254EEB25477B68fb85Ed929f73A960582",
+          "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+          "0x0168B5FcB54F662998B0620b9365Ae027192621f",
+          "e11192612ceb48108b4f2730a9ddbea3",
+          "0eb8d4b227f7486580b6f66706ac5d47",
+        ]
       ]
     }
   });
