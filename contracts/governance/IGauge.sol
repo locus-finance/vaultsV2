10997c3eaa202f8028cd3c97d3bb12213cf0b566
GoldenSylph <simplavero@gmail.com>
2023-10-09 17:28:34 +0400

    remove staking

--- contracts/governance/IGauge.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
-
-interface IGauge {
-    function integrateFraction(address addr) external view returns (uint256);
-    function userCheckpoint(address user) external returns (bool);
-}
