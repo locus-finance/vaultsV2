eca0d60872b911b9c0903981340b0c1df4660c80
GoldenSylph <simplavero@gmail.com>
2023-11-17 16:07:06 +0400

    modifying upgrade script

--- scripts/upgradeAny.js
+++ scripts/upgradeAny.js
@@ -2,9 +2,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  const TARGET_ADDRESS = "0x0427eE06e5220BA8013d2A753109A57AD4020373";
-  const TARGET_STRATEGY = "HopStrategy";
-  //   console.log(1);
+  const TARGET_ADDRESS = "0xD6D7673D94BAcDD1FA3D67D38B5A643Ba24F85b3";
+  const TARGET_STRATEGY = "BeefyStrategy";
+
   const TargetContract = await hre.ethers.getContractFactory(TARGET_STRATEGY);
 
   const upgraded = await hre.upgrades.upgradeProxy(
@@ -13,6 +13,23 @@ async function main() {
   );
 
   console.log("Successfully upgraded implementation of", upgraded.address);
+
+  const strategyInstance = await hre.ethers.getContractAt(
+    TARGET_STRATEGY,
+    TARGET_ADDRESS
+  );
+
+  // const setManagementFeeTx = await strategyInstance.setManagementFee(7000);
+  // await setManagementFeeTx.wait();
+
+  // const setStrategistTx = await strategyInstance.setStrategist("0x27f52fd2E60B1153CBD00D465F97C05245D22B82");
+  // await setStrategistTx.wait();
+  
+  // const setTreasuryAddressTx = await strategyInstance.setTreasuryAddress("0xf4bec3e032590347fc36ad40152c7155f8361d39");
+  // await setTreasuryAddressTx.wait();
+
+  const setVaultTx = await strategyInstance.setVault("0xf712eE1C45C84aEC0bfA1581f34B9dc9a54D7e60", 110);
+  await setVaultTx.wait();
 
   //   await hre.run("verify:verify", {
   //     address: upgraded.address,
