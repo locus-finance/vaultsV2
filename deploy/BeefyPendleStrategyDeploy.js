258c560753e5b745851907cc86d099fe22f658b1
Oleg Bedrin <numeralhuman@gmail.com>
2024-07-21 22:34:03 +0400

    adding test fixture

--- deploy/BeefyPendleStrategyDeploy.js
+++ deploy/BeefyPendleStrategyDeploy.js
@@ -3,7 +3,32 @@ const hre = require("hardhat");
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const config = bridgeConfig[hre.network.name];
   const vaultConfig = bridgeConfig[vaultChain(hre.network.name)];
-  const BeefyPendleStrategy = await ethers.getContractFactory("BeefyPendleStrategy");
+
+  const libraryNames = ["BeefyPendleStrategyLib"];
+
+  const libraries = {};
+  if (libraryNames !== undefined) {
+    console.log('Found libraries to deploy and link!');
+    for (const libraryName of libraryNames) {
+      const library = await hre.ethers.deployContract(libraryName);
+      console.log(`Deployed library: ${libraryName} - ${library.address}`);
+      libraries[libraryName] = await library.getAddress();
+      await hre.run("verify:verify", {
+        address: await library.getAddress()
+      });
+    }
+  } else {
+    console.log("No external libraries for this strategy. Continue...");
+  }
+
+  let factoryParams;
+  if (libraryNames.length > 0) {
+    factoryParams = {
+      libraries
+    }
+  }
+  const BeefyPendleStrategy = await ethers.getContractFactory("BeefyPendleStrategy", factoryParams);
+
   const beefyPendleStrategy = await upgrades.deployProxy(
     BeefyPendleStrategy,
     [
@@ -20,6 +45,9 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     {
       initializer: "initialize",
       kind: "uups",
+      unsafeAllow: [
+        "external-library-linking"
+      ]
     }
   );
   await beefyPendleStrategy.waitForDeployment();
--- test/strategies/BeefyPendleStrategyTest.js
+++ test/strategies/BeefyPendleStrategyTest.js
@@ -1,12 +1,12 @@
 const { expect } = require("chai");
 const { utils } = require("ethers");
 const hre = require("hardhat");
-const { ethers, deployments, getNamedAccounts } = hre;
-const { get } = deployments;
+const { ethers } = hre;
+const {
+  loadFixture
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 describe("BeefyPendleStrategy", function () {
-  let deployer;
-
   const mintNativeTokens = async (signer, amountHex) => {
     await hre.network.provider.send("hardhat_setBalance", [
       signer.address || signer,
@@ -28,13 +28,212 @@ describe("BeefyPendleStrategy", function () {
     });
   };
 
+  async function deployFixture() {
+    const wantToken = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
+    const sgRouter = "0x0000000000000000000000000000000000000001";
+    const chainId = 110;
+    const TOKENS = {
+      USDC: {
+        address: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+        whale: "0x62383739d68dd0f844103db8dfb05a7eded5bbe6",
+        decimals: 6,
+      },
+      ETH: {
+        whale: "0xb38e8c17e38363af6ebdcb3dae12e0243582891d",
+      },
+    };
+
+    async function dealTokensToAddress(
+      address,
+      dealToken,
+      amountUnscaled = "100"
+    ) {
+      const token = await ethers.getContractAt("IERC20", dealToken.address);
+
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [dealToken.whale],
+      });
+      const tokenWhale = await ethers.getSigner(dealToken.whale);
+
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [TOKENS.ETH.whale],
+      });
+      const ethWhale = await ethers.getSigner(TOKENS.ETH.whale);
+
+      await ethWhale.sendTransaction({
+        to: tokenWhale.address,
+        value: ethers.parseEther("50"),
+      });
+
+      await token
+        .connect(tokenWhale)
+        .transfer(address, ethers.parseUnits(amountUnscaled, dealToken.decimals));
+    }
+
+    const [deployer, governance] = await ethers.getSigners();
+    const want = await ethers.getContractAt("IERC20", wantToken);
+
+    const BridgeMock = await ethers.getContractFactory("BridgeMock");
+    const bridgeMock = await BridgeMock.deploy();
+    await bridgeMock.waitForDeployment();
+
+    const LZEndpointMock = await ethers.getContractFactory("LZEndpointMock");
+    const lZEndpointMock = await LZEndpointMock.deploy();
+    await lZEndpointMock.waitForDeployment();
+
+    const StargateMock = await ethers.getContractFactory("StargateMock");
+    const stargateMock = await StargateMock.deploy();
+    await stargateMock.waitForDeployment();
+
+    const SgBridge = await ethers.getContractFactory("SgBridge");
+    const sgBridge = await upgrades.deployProxy(
+      SgBridge,
+      [
+        sgRouter,
+        chainId
+      ],
+      {
+        initializer: "initialize",
+        kind: "transparent",
+      }
+    );
+    await sgBridge.waitForDeployment();
+    await sgBridge.setStargatePoolId(wantToken, 110, 1);
+
+    const Vault = await ethers.getContractFactory("Vault");
+    console.log(governance.address, await lZEndpointMock.getAddress());
+    const vault = await upgrades.deployProxy(
+      Vault,
+      [
+        governance.address,
+        deployer.address,
+        await lZEndpointMock.getAddress(),
+        wantToken,
+        sgRouter,
+      ],
+      {
+        initializer: "initialize",
+        kind: "transparent",
+      }
+    );
+    await vault.waitForDeployment();
+
+    const governanceRole = "0x35a7846a2a701fff6f9d61a46ebff5da578c5dcee8bdf361c569f9ea4ee64771"; // keccak256("GOVERNANCE")
+    const grantRoleTx = await vault.grantRole(governanceRole, deployer.address);
+    await grantRoleTx.wait();
+
+    const libraryNames = ["BeefyPendleStrategyLib"];
+
+    const libraries = {};
+    if (libraryNames !== undefined) {
+      console.log('Found libraries to deploy and link!');
+      for (const libraryName of libraryNames) {
+        const library = await ethers.deployContract(libraryName);
+        const libAddress = await library.getAddress();
+        console.log(`Deployed library: ${libraryName} - ${libAddress}`);
+        libraries[libraryName] = libAddress;
+      }
+    } else {
+      console.log("No external libraries for this strategy. Continue...");
+    }
+
+    let factoryParams;
+    if (libraryNames.length > 0) {
+      factoryParams = {
+        libraries
+      }
+    }
+    const BeefyPendleStrategy = await ethers.getContractFactory("BeefyPendleStrategy", factoryParams);
+    const strategy = await upgrades.deployProxy(
+      BeefyPendleStrategy,
+      [
+        await lZEndpointMock.getAddress(),
+        deployer.address,
+        deployer.address,
+        wantToken,
+        await vault.getAddress(),
+        110,
+        110,
+        await sgBridge.getAddress(),
+        sgRouter
+      ],
+      {
+        initializer: "initialize",
+        kind: "uups",
+        unsafeAllow: [
+          "external-library-linking"
+        ]
+      }
+    );
+    await strategy.waitForDeployment();
+
+    const VaultToken = await ethers.getContractFactory("VaultToken");
+    const vaultToken = await upgrades.deployProxy(
+      VaultToken,
+      [
+        deployer.address,
+        await vault.getAddress()
+      ],
+      {
+        initializer: "initialize",
+        kind: "uups",
+      }
+    );
+    await vaultToken.waitForDeployment();
+
+    const setSgLzSettingsTx = await vault.setSgLzSettings(
+      await bridgeMock.getAddress(),
+      await sgBridge.getAddress(),
+      9000000
+    );
+    await setSgLzSettingsTx.wait();
+
+    const setDepositLimitTx = await vault.setDepositLimit(ethers.parseUnits("10000000", 6));
+    await setDepositLimitTx.wait();
+
+    const setVaultTokenTx = await vault.setVaultToken(await vaultToken.getAddress());
+    await setVaultTokenTx.wait();
+
+    await vault["addStrategy(uint16,address,uint256,uint256,address)"](
+      110,
+      await strategy.getAddress(),
+      10000,
+      0,
+      deployer.address
+    );
+
+    await dealTokensToAddress(deployer.address, TOKENS.USDC, "1000");
+
+    const ethWhale = await ethers.getSigner(TOKENS.ETH.whale);
+
+    await ethWhale.sendTransaction({
+      to: await vault.getAddress(),
+      value: ethers.parseEther("1"),
+    });
+    await ethWhale.sendTransaction({
+      to: await strategy.getAddress(),
+      value: ethers.parseEther("1"),
+    });
+
+    return { sgBridge, strategy, vault, deployer, want };
+  }
+
+  let sgBridge;
+  let strategy;
+  let vault;
+  let deployer;
+
   beforeEach(async function () {
-    await deployments.fixture(["MockSwapHelperSubscriber", "SwapHelper"]);
-    const accounts = await getNamedAccounts();
-    deployer = accounts.deployer;
+    const fixtureData = await loadFixture(deployFixture);
+    sgBridge = fixtureData.sgBridge;
+    strategy = fixtureData.strategy;
+    vault = fixtureData.vault;
+    deployer = fixtureData.deployer;
   });
 
   it('should', async () => {
-
+    console.log('do smth');
   });
 });
